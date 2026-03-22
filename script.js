ab325abd1f068aaf5819eed55c772d677272fa8c
Chaisson <Chaissonrcook@gmail.com>
2026-03-22 13:41:32 -0700

    Pawfect Beauty V2.1
    -Added About Section
    -Added Other Pages

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
 
-    const track = document.querySelector('.dog-track');
-    const next = document.querySelector('#dogNext');
-    const prev = document.querySelector('#dogPrev');
-    const dogCount = document.querySelectorAll('.dog-track img').length;
-    
-    let index = 0;
-    const dogsVisible = 7;
+const track = document.querySelector('.dog-track');
+const images = Array.from(track.querySelectorAll('img'));
 
-    next.addEventListener('click', () => {
-        // Stop sliding when we reach the last set of 7
-        if (index < dogCount - dogsVisible) {
-            index++;
-            updateSlide();
-        } else {
-            index = 0; // Loop back to start
-            updateSlide();
-        }
-    });
-
-    prev.addEventListener('click', () => {
-        if (index > 0) {
-            index--;
-            updateSlide();
-        }
-    });
-
-    function updateSlide() {
-        const movePercentage = index * (100 / dogsVisible);
-        track.style.transform = `translateX(-${movePercentage}%)`;
-    }
+// Clone images for seamless infinite loop
+images.forEach(img => {
+    track.appendChild(img.cloneNode(true));
+});
